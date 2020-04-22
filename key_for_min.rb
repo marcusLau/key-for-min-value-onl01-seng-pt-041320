38e25f8558f8f9e66acd6ff5a50dcca11e3671b3
Marcus Lau <lau.marcus76@gmail.com>
2020-04-22 22:37:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  low_key = ""
   if name_hash.size == 0 return nil
   name_hash.collect do |name, value|
-    
+    if low_key == "" 
+      low_key = name
+    else 
+      if value < name_hash[low_key]
+        low_key = name
+      end
+    end
+  end
+  return low_key
 end
